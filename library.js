0208cd78ce7edae9191900f817b6774610765093
Muhammad <areebqureshi1122@gmail.com>
2024-07-11 21:52:53 +0500

    Javascript Journey

---
+++ library.js
@@ -0,0 +1,17 @@
+// export default function(){
+//     console.log("hello")
+// }
+
+
+//  let  message = "ES6 Modules";
+
+ function user(name){
+    return `hello ${name}`
+}
+// class test {
+//     constructor(){
+//         console.log("Constructor Method")
+//     }
+// }
+
+ export {  user}
---
+++ main.js
@@ -0,0 +1,15 @@
+// import areeb from "./library.js"
+import { user}from "./bridge.js"
+
+console.log(user("Muhammad Areeb"));
+// areeb();
+
+// console.log(message)
+// console.log(user("Muhammad areeb"))
+// import * as modules1 from "./library.js";
+
+// console.log(modules1.message);
+// // document.body.innerHTML = message;
+// console.log(modules1.user("Muhammad Areeb"))
+
+// let a = new modules1.test();
