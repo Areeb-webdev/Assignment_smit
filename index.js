6922b83a3214f8a9983d29a33df1c5fc539a53e2
Muhammad <areebqureshi1122@gmail.com>
2024-08-19 10:29:31 +0500

    Mern assigment no:5

---
+++ index.js
@@ -0,0 +1,148 @@
+                                        // assigmnet no 5 ;
+  // write a program that take two numbers & add them in a new variable. 
+  // show the result in your browser.
+
+//       let a = 10;
+//       let b = 32;
+//       let c = a + b;
+//       document.write(`Sum of ${a} and ${b} is: <b>${c}</b> <br><br>`);
+
+// repeat task1 for subtraction, multiplication, division, & modulus.
+
+
+//       let a1 = 150;
+//       let b1 = 42;
+//       let c1 = a1 - b1;
+//       document.write(
+//         `Subtraction of ${a1} and ${b1} is: <b>${c1}</b> <br><br>`
+//       );
+
+//       let a2 = 4;
+//       let b2 = 2;
+//       let c2 = a2 * b2;
+//       document.write(
+//         `Multiplication of ${a2} and ${b2} is: <b>${c2}</b> <br><br>`
+//       );
+
+//       let a3 = 40;
+//       let b3 = 4;
+//       let c3 = a3 / b3;
+//       document.write(`Division of ${a3} and ${b3} is: <b>${c3}</b> <br><br>`);
+
+//       let a4 = 10;
+//       let b4 = 32;
+//       let c4 = a4 % b4;
+//       document.write(`Modulas of ${a4} and ${b4} is: <b>${c4}</b> <br><br>`);
+//             // { addition, subtraction, multiplication, division, modulas task complete.}
+
+// Task 2
+//         let var1 ;// declare a variable 
+
+
+//         document.write("Value after variable declaration is: " + var1 + "<br><br>");
+
+//         var var2 = 5 ;
+//         document.write("Initial value: " + var2 + "<br><br>");
+
+//         var var3 = var2 + 1 ;
+//         document.write("Value Of  5 after increment is: " + var3 + "<br><br>");
+
+//         var var4 = var3 + 7 ;
+//         document.write("Value  after addition is: " + var4 + "<br><br>");
+
+//         var var5 = --var4 ;
+//         document.write("Value  after decrement is: " + var5 + "<br><br>");
+
+        // var remindervar = var5 % 3 ;
+        // document.write("The remainder is: " + remindervar + "<br><br>");
+                                                                            // Ticket Task
+//                 var Ticket = 600; //ticket price
+//                 var calculate_ticket = Ticket * 5;
+//                 document.write("<b>Ticket Task</b><br><br>")
+//                 document.write("Total cost to buy  5 tickets is: " + calculate_ticket)
+
+// write a program that display multiplication table of any number in your browser.
+//   function table(num){
+//   for(i=1; i<=10; i++){
+//     document.write(num + " * "+ i + " = " + num*i + "<br>" )
+//   }
+//  }table(5);
+
+                                        // temperature converter
+                                        
+          // var celsius  = 44 ;
+          // var celsiusToFahrenheit = (celsius * 9/5) +32;
+          // document.write(` ${celsius}"C" "is" ${celsiusToFahrenheit}F <br>`);
+
+          // var fahrenheit  = 144 ;
+          // var FahrenheitToCelsius = (fahrenheit- 32)* 5/9;
+          // document.write(` ${fahrenheit}"F" "is" ${FahrenheitToCelsius}C <br> `)
+
+          // checkout process of ecommerce store
+
+          // var priceOfItem1 = 650;
+          // var priceOfItem2 = 100 ;
+          // var orderedQuantityOfItem1 = 3 ;
+          // var orderedQuantityOfItem2  = 7;
+          // var shippingCharges = 100;
+          // var totalCost = ( ( priceOfItem1 * orderedQuantityOfItem1 ) + (priceOfItem2 * orderedQuantityOfItem2)+ shippingCharges)
+          // document.getElementById("heading").innerHTML = "Shopping Cart";
+          // document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+          // document.write("Quantity of item 1 is "+ orderedQuantityOfItem1 + "<br>");
+          // document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+          // document.write("Quantity of item 2 is "+ orderedQuantityOfItem2 + "<br>");
+          // document.write("Shipping charges "+ shippingCharges + "<br>"+ "<br>");
+          // document.write("Total cost of your order is "+ totalCost);
+          
+          // 2 students marsheet
+
+          // var totalMarks = 950 ;
+          // var obtainedMarks = 800;
+          // var percentage = obtainedMarks/ totalMarks * 100 ;
+          // document.write("Marks Sheet" + "<br>"+ "<br>")
+          // document.write("Total marks:" + totalMarks + "<br>");
+          // document.write("Otained marks:" + obtainedMarks + "<br>");
+          // document.write("Percentage:" + percentage );
+
+                                            // currency converter
+  //   var usDollar = 104.80 * 10 
+  //   var saudiRiyal = 28 *25  ;
+  //   var totalCurrency = usDollar + saudiRiyal ;
+  //  document.write("Total Currency in PKR: " + totalCurrency)
+
+  // declare a variable and do arithmetic on the vaiable
+//   let num = 10;
+// num = (((((num + 5) * 2) - 3) / 2) % 3);
+
+//   document.write(num)
+          // age calculator
+
+    // var currentYear = 2024 ;
+    // var birthYear = 2002 ;
+    // var age = currentYear - birthYear ;
+    // document.write("Current year: " + currentYear + "<br>");
+    // document.write("Birth year: " + birthYear + "<br>");
+    // document.write("your Age is : " + age);
+
+    // radius geometrics 
+    // var radius = 20 ;
+    // var area  = 3.142 *(2*2);
+    // var circumference  = 2 * 3.142 * radius ;
+    // document.write("Radius of a circle: "+ radius + "<br>")
+    // document.write("The circumference is : " + circumference + "<br>")
+    // document.write("The area of circle is : " + area)
+
+  // lifetime supply of your favorite snack ;
+  // var snack ="chocolate chip";
+  // var currentAge = 22 ;
+  // var maxAge = 70 ;
+  // var perDaySnack = 2 ;
+  // var total = 2 * 365 * 48 + 12;
+  // document.write("Favourite Snack:" + snack + "<br>");
+  // document.write("Current age:" + currentAge + "<br>");
+  // document.write("Estimated Maximum Age:" + maxAge + "<br>");
+  // document.write("Amount of snacks per day:" + perDaySnack + "<br>");
+  // document.write("You will need " + total+ " snacks to last you until the ripe old age of " + maxAge)
+
+    
+// Chapter five is completed here.
